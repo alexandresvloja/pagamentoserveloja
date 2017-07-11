486bb72349ce30b4642456583867e1c0e3411ea1
alexandre <alexandre@serveloja.com.br>
2017-07-11 08:56:27 -0300

    método para efetuar transação com token

--- app/src/main/java/br/com/servelojapagamento/webservice_mundipagg/BaseMundipaggAPI.java
+++ app/src/main/java/br/com/servelojapagamento/webservice_mundipagg/BaseMundipaggAPI.java
@@ -16,7 +16,7 @@ public interface BaseMundipaggAPI {
     @FormUrlEncoded
     @POST("transacao/criarTransacaoSemToken")
     Call<RespostaTransacaoMundipagg> criarTransacaoSemToken(@Field("valor") String valor,
-                                                            @Field("id_transacao") String idTransacao,
+                                                            @Field("ref_pedido") String refPedido,
                                                             @Field("cartao_bandeira") String cartaoBandeira,
                                                             @Field("cartao_numero") String cartaoNumero,
                                                             @Field("cartao_cvv") String cartaoCvv,
@@ -24,6 +24,13 @@ public interface BaseMundipaggAPI {
                                                             @Field("cartao_mes") String cartaoMes,
                                                             @Field("cartao_nome") String cartaoNome,
                                                             @Field("numero_parcelas") int numParcelas);
+
+    @FormUrlEncoded
+    @POST("transacao/criarTransacaoComToken")
+    Call<RespostaTransacaoMundipagg> criarTransacaoComToken(@Field("valor") String valor,
+                                                            @Field("ref_pedido") String refPedido,
+                                                            @Field("token") String token,
+                                                            @Field("numero_parcelas") String numParcelas);
 
     @GET("transacao/consultarTransacaoPorChavePedido")
     Call<RespostaTransacaoMundipagg> consultarTransacaoPorChavePedido(@Query("chave") String chave);
@@ -38,19 +45,19 @@ public interface BaseMundipaggAPI {
     @FormUrlEncoded
     @POST("token/criarToken")
     Call<RespostaCriarToken> criarToken(@Field("cartao_bandeira") String cartaoBandeira,
-                                                @Field("cartao_numero") String cartaoNumero,
-                                                @Field("cartao_mes") String cartaoMes,
-                                                @Field("cartao_ano") String cartaoAno,
-                                                @Field("cartao_nome") String cartaoNome,
-                                                @Field("cartao_check") boolean cartaoCheck,
-                                                @Field("endereco_descricao") String enderecoDescricao,
-                                                @Field("endereco_numero") String endercoNumero,
-                                                @Field("endereco_complemento") String enderecoComplemento,
-                                                @Field("endereco_capital") String enderecoCapital,
-                                                @Field("endereco_cidade") String enderecoCidade,
-                                                @Field("endereco_estado") String enderecoEstado,
-                                                @Field("endereco_cep") String enderecoCep,
-                                                @Field("endereco_pais") String enderecoPais);
+                                        @Field("cartao_numero") String cartaoNumero,
+                                        @Field("cartao_mes") String cartaoMes,
+                                        @Field("cartao_ano") String cartaoAno,
+                                        @Field("cartao_nome") String cartaoNome,
+                                        @Field("cartao_check") boolean cartaoCheck,
+                                        @Field("endereco_descricao") String enderecoDescricao,
+                                        @Field("endereco_numero") String endercoNumero,
+                                        @Field("endereco_complemento") String enderecoComplemento,
+                                        @Field("endereco_capital") String enderecoCapital,
+                                        @Field("endereco_cidade") String enderecoCidade,
+                                        @Field("endereco_estado") String enderecoEstado,
+                                        @Field("endereco_cep") String enderecoCep,
+                                        @Field("endereco_pais") String enderecoPais);
 
     @GET("token/consultarToken")
     Call<RespostaConsultarToken> consultarToken(@Query("token") String token);
